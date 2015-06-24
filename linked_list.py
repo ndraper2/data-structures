26676133e57dd08cf410e92dd69afdb414f71d49
Saki Fu <sakifu.86@gmail.com>
2015-06-24 16:52:35 -0700

    add two files

---
+++ linked_list.py
@@ -0,0 +1,56 @@
+
+
+class Node(object):
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+class LinkedList(object):
+    def __init__(self, iterable=None):
+        self.size = 0
+        self.head = None
+        if iterable:
+            for item in iterable:
+                self.insert(item)
+
+    def insert(self, value):
+        self.head = Node(value, self.head)
+        self.size += 1
+
+    def pop(self, value):
+        return_val = self.head.value
+        self.head = self.head.next
+        self.size -= 1
+        return return_val
+
+    def size(self):
+        return self.size
+
+    def search(self, value):
+        iter_node = self.head
+        while iter_node:
+            if iter_node.value == value:
+                return iter_node
+            iter_node = iter_node.next
+        return None
+
+    def remove(self, node):
+        if self.head == node:
+            self.head = self.head.next
+        else:
+            iter_node = self.head
+            while iter_node.next:
+                if iter_node.next == node:
+                    iter_node.next = iter_node.next.next
+            raise ValueError('node not in list')
+
+    def display(self):
+        temp_list = []
+        iter_node = self.head
+        while iter_node:
+            temp_list.append(iter_node.value)
+            iter_node = iter_node.next
+        return tuple(temp_list)
+
+
