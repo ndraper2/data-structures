d09d5de4059dae573247ba54803c6158ef4a7b1c
Saki Fu <sakifu.86@gmail.com>
2015-06-29 13:23:56 -0700

    add queue.py and test for it.

--- queue.py
+++ queue.py
@@ -4,6 +4,7 @@ from linked_list import Node
 
 
 class Queue(object):
+    """Create a new Queue with optional given iterable of values."""
     def __init__(self, iterable=None):
         self._size = 0
         self.head = None
@@ -12,7 +13,8 @@ class Queue(object):
             for item in iterable:
                 self.enqueue(item)
 
-    def enqueue(self, value):  #adds value to the queue
+    def enqueue(self, value):
+        """Create a new node and enqueue it at the head of the queue."""
         if not self.head:
             self.head = Node(value)
             self.tail = self.head
@@ -22,11 +24,13 @@ class Queue(object):
             self.head = self.head.next
             self._size += 1
 
-    def dequeue(self):  #removes the correct item from the queue and returns its value
+    def dequeue(self):
+        """Remove the tail node from the queue and return its value."""
         value = self.tail.value
         self.tail = self.tail.next
         self._size -= 1
         return value
 
     def size(self):
+        """Return the size of the queue."""
         return self._size
