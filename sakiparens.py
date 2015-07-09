2d362d62ff262943cdf0bb57ad9a67d13eb229ec
Saki Fu <sakifu.86@gmail.com>
2015-07-09 13:12:59 -0700

    edited

--- sakiparens.py
+++ sakiparens.py
@@ -8,10 +8,15 @@ def parens(string):
         elif char == ')':
             try:
                 stack.pop()
-            except:
+            except IndexError:
                 return -1
 
-    if len(stack) == 0:
+    # if len(stack) == 0:
+    #     return 0
+    # if len(stack) > 0:
+    #     return 1
+    try:
+        stack.pop()
+    except IndexError:
         return 0
-    if len(stack) > 0:
-        return 1
+    return 1
